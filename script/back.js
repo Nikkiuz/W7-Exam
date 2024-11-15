603904fbf75f7645b5ec1c645c9a6d7a18676e28
Niccolò Albanese <nick_albanese@virgilio.it>
2024-11-15 09:10:41 +0100

    setup

---
+++ script/back.js
@@ -0,0 +1,8 @@
+//fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+//headers: {
+//    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MDA4NjhhZDEyOTAwMTU4NzZiYjAiLCJpYXQiOjE3MzE2NTc4NjIsImV4cCI6MTczMjg2NzQ2Mn0.yUJnuzj1z4LWUFNs5M3xsZG8AUxLupnaVzzFpUjSB0o"
+//    }
+//    })
+
+const myUrl = 'https://striveschool-api.herokuapp.com/api/product/,'
+const apikey = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MDA4NjhhZDEyOTAwMTU4NzZiYjAiLCJpYXQiOjE3MzE2NTc4NjIsImV4cCI6MTczMjg2NzQ2Mn0.yUJnuzj1z4LWUFNs5M3xsZG8AUxLupnaVzzFpUjSB0o'
